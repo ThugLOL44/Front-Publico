5bf52a8b161e8cf686c85c8f4c315b7d0ad74ec3
Gonzalo Corsiglia <gonza.corsiglia@gmail.com>
2023-05-17 22:41:02 -0300

    Home hecho faltan ver detalles, empezaria ya a codear el menu

--- FrontEnd TP3/slider.js
+++ FrontEnd TP3/slider.js
@@ -1,9 +1,31 @@
 var counter = 1;
+var interval = 5000;
 setInterval(function(){
+
+  document.getElementById('radio1').addEventListener('click', function() {
+    if (this.checked) {
+      counter = 1;
+    }
+  });
+  document.getElementById('radio2').addEventListener('click', function() {
+    if (this.checked) {
+      counter = 2;
+    }
+  });
+  document.getElementById('radio3').addEventListener('click', function() {
+    if (this.checked) {
+      counter = 3;
+    }
+  });
+  document.getElementById('radio4').addEventListener('click', function() {
+    if (this.checked) {
+      counter = 4;
+    }
+  });
   document.getElementById('radio' + counter).checked = true;
   counter++;
   if(counter > 4){
     counter = 1;
   }
-}, 5000);
+}, interval);
 
